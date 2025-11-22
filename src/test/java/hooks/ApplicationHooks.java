aeaf66f70f923756730c06609db4357b3fd6a984
Shubham <Shubhamsep23@gmail.com>
2025-11-23 00:08:30 +0530

    23-11-2025 Commit

--- src/test/java/hooks/ApplicationHooks.java
+++ src/test/java/hooks/ApplicationHooks.java
@@ -1,4 +1,16 @@
-package Hooks;
+package hooks;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 
 public class ApplicationHooks {
+    @Before
+    public void setup() {
+        System.out.println("Setup before Scenario");
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Clean up Scenario");
+    }
 }
